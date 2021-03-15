961ab0d4aee34874881306929698f4f23485e2e5
doberman7 <ivanrubioangeles@gmail.com>
2021-03-15 16:38:13 -0600

    obj

--- problema2/problema2.rb
+++ problema2/problema2.rb
@@ -1,6 +1,29 @@
 file = File.open("Input.txt")
 file_data = file.readlines.map(&:chomp)
 
-# puts file_data
 
-File.open("Output.txt", "w") { |f| f.write "#{Time.now} - User logged in\n" }
+class Marcador
+  def initialize(p1, p2)
+    @index, @data = p1, p2
+  end
+end
+
+marcadores = []
+
+file_data.each_with_index do |data,index|
+    case index
+        when 0
+        rondas = data
+        when 1..rondas
+        marc = Marcador.new(index, data)    
+        marcadores.push(marc)
+        else
+        "outside of range"
+    end 
+end
+
+puts "#{marcadores}"
+
+
+# writte 
+# File.open("Output.txt", "w") { |f| f.write "#{file_data} - User logged in\n" }
