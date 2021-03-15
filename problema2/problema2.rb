413e618cfdc4438598c4e7b10ad49cbd7e174f17
doberman7 <ivanrubioangeles@gmail.com>
2021-03-15 14:32:03 -0600

    write

--- problema2/problema2.rb
+++ problema2/problema2.rb
@@ -1,4 +1,6 @@
 file = File.open("Input.txt")
 file_data = file.readlines.map(&:chomp)
 
-puts file_data
+# puts file_data
+
+File.open("Output.txt", "w") { |f| f.write "#{Time.now} - User logged in\n" }
