bb02fd0400c902fe5893abfc17b48b13b64d8e39
doberman7 <ivanrubioangeles@gmail.com>
2021-03-15 11:31:21 -0600

    on ary

--- app.js
+++ app.js
@@ -6,17 +6,23 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+  //   res.setHeader("Content-Type", "text/plain");
+  //   res.end("Hello World");
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  //   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 fs.readFile("Input.txt", (err, data) => {
   if (err) throw err;
-
-  console.log(data.toString());
+  let datos = data.toString();
+  analize(datos);
   server.close();
 });
+
+analize = (data) => {
+  //   console.log(data);
+  let newstrings = data.split("\n");
+  console.log(newstrings);
+};
