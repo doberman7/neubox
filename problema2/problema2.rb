7d530438c182aa181ec7a35dab611427c437b96e
doberman7 <ivanrubioangeles@gmail.com>
2021-03-15 18:01:05 -0600

    write

--- problema2/problema2.rb
+++ problema2/problema2.rb
@@ -1,7 +1,7 @@
 file = File.open("Input.txt")
 file_data = file.readlines.map(&:chomp)
 
-
+# clases
 class Marcador
   #   to access atributes with .dot nomenclature
   attr_accessor :ronda, :p1Points, :p2Points, :lider, :ventaja
@@ -10,7 +10,7 @@ class Marcador
     @ronda, @p1Points , @p2Points, @lider, @ventaja = ronda, p1Points, p2Points, lider, ventaja
   end
 end
-
+# metodos
 def getAryOfMarcadorObjects(arch)
     marcadores = []
         arch.each_with_index do |data,index|  
@@ -23,11 +23,11 @@ def getAryOfMarcadorObjects(arch)
                     dataP2 = points[1]
                      # determine winner of ronda    
                     if dataP1.to_i > dataP2.to_i
-                        lider = "P1"
+                        lider = "1"
                         advantage = dataP1.to_i - dataP2.to_i
                         marc = Marcador.new(index, dataP1, dataP2, lider ,advantage)    
                     elsif dataP1.to_i < dataP2.to_i
-                        lider = "P2"
+                        lider = "2"
                         advantage =  dataP2.to_i - dataP1.to_i 
                         marc = Marcador.new(index, dataP1, dataP2, lider,advantage)        
                     end
@@ -48,8 +48,5 @@ end
 
 aryMarcadores = getAryOfMarcadorObjects(file_data)
 winnerMarcador = theWinnerIs(aryMarcadores)
-p winnerMarcador.lider
-p winnerMarcador.ventaja
-
 # writte 
-File.open("Output.txt", "w") { |f| f.write "#{winnerMarcador.lider} \n #{winnerMarcador.ventaja}" }
+File.open("Output.txt", "w") { |f| f.write "#{winnerMarcador.lider} #{winnerMarcador.ventaja}" }
